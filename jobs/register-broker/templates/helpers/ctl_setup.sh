17a2fac4612c449bd13880c1ec437efdb3e1425c
Samuel Marks <samuelmarks@gmail.com>
2016-02-18 00:33:43 +1100

    Fixed mongod deployment, implementing cf-service-broker errand

--- jobs/register-broker/templates/helpers/ctl_setup.sh
+++ jobs/register-broker/templates/helpers/ctl_setup.sh
@@ -14,36 +14,27 @@ set -e # exit immediately if a simple command exits with a non-zero status
 set -u # report the usage of uninitialized variables
 
 JOB_NAME=$1
-output_label=${1:-JOB_NAME}
+output_label=${2:-${JOB_NAME}}
 
 export JOB_DIR=/var/vcap/jobs/$JOB_NAME
 chmod 755 $JOB_DIR # to access file via symlink
-chmod 600 $JOB_DIR/config/mongodb-keyfile
-
-# Load some bosh deployment properties into env vars
-# Try to put all ERb into data/properties.sh.erb
-# incl $NAME, $JOB_INDEX, $WEBAPP_DIR
-source $JOB_DIR/data/properties.sh
 
 source $JOB_DIR/helpers/ctl_utils.sh
-redirect_output ${output_label}
 
-export HOME=${HOME:-/home/vcap}
+# Add all packages' /bin & /sbin into $PATH
+for package_bin_dir in $(ls -d /var/vcap/packages/*/*bin)
+do
+  export PATH=${package_bin_dir}:$PATH
+done
 
-# Add all binary packages into $PATH
-
-# for package_bin_dir in $(find /var/vcap/data/packages -type f -executable -printf "%h\n" | uniq)
-# do
-#   export PATH=${package_bin_dir}:$PATH
-# done
-
-declare -A seen_dir
-while read -rd '' f; do
-    seen_dir["${f%/*}"]=1
-done < <(find "/var/vcap/data/packages" -type f -executable -print0)
-export PATH="${!seen_dir[@]}:$PATH"
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-''} # default to empty
+for package_bin_dir in $(ls -d /var/vcap/packages/*/lib)
+do
+  export LD_LIBRARY_PATH=${package_bin_dir}:$LD_LIBRARY_PATH
+done
 
 # Setup log, run and tmp folders
+
 export RUN_DIR=/var/vcap/sys/run/$JOB_NAME
 export LOG_DIR=/var/vcap/sys/log/$JOB_NAME
 export TMP_DIR=/var/vcap/sys/tmp/$JOB_NAME
@@ -56,4 +47,7 @@ do
 done
 export TMPDIR=$TMP_DIR
 
-PIDFILE=$RUN_DIR/$JOB_NAME.pid
+export C_INCLUDE_PATH=/var/vcap/packages/mysqlclient/include/mysql:/var/vcap/packages/sqlite/include:/var/vcap/packages/libpq/include
+export LIBRARY_PATH=/var/vcap/packages/mysqlclient/lib/mysql:/var/vcap/packages/sqlite/lib:/var/vcap/packages/libpq/lib
+
+printf '$PATH=%s' "$PATH"
